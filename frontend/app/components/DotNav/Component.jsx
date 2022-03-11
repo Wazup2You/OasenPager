578f533bc897e7f246642a0d3698b941e9ae57a8
Weifang <wazup2you@gmail.com>
2022-03-11 09:57:30 +0100

    Changed size Dotnav dots

--- frontend/app/components/DotNav/Component.jsx
+++ frontend/app/components/DotNav/Component.jsx
@@ -7,9 +7,10 @@ export function DotNav (props) {
 		<>
 			<ul className='fixed top-1/2 right-5 -translate-y-1/2 z-50' >
 				{props.sections.map((section, index) =>
-					<li className={`${props.activeState === index ? 'opacity-100 bg-white scale-200' : 'opacity-50'} rounded-full cursor-pointer bg-white flex justify-center items-center p-1 mb-2 border-2 border-transparent`} key={section} aria-label={props.activeState === index ? 'active section' : 'inactive section'}>
-						<Link to={`#${section}`} className=' relative top-0 w-2 h-2'></Link>
+				<Link to={`#${section}`} className=' relative top-0 w-2 h-2'>
+					<li className={`${props.activeState === index ? 'opacity-100 bg-white scale-200' : 'opacity-50'} rounded-full cursor-pointer bg-white flex justify-center items-center p-1 mb-2 border-2 border-transparent h-5 w-5`} key={section} aria-label={props.activeState === index ? 'active section' : 'inactive section'}>
 					</li>
+					</Link>
 				)}
 			</ul>
 		</>
